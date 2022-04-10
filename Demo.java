6a7892e7336391f63d2cb75b38c800864ef67919
Sangram Shinde <sangramshinde0105@gmail.com>
2022-04-10 16:04:28 +0530

    Mock1 test added

---
+++ Demo.java
@@ -0,0 +1,35 @@
+package Accesspackage;
+
+public class Demo {
+
+
+	//this is default so it cannot be used outside package
+	static void m1() {
+		
+		System.out.println("Static method in demo class");
+	}
+	
+	void m2()  {
+		
+		System.out.println("Non-Static method in demo class");
+		
+		
+	}
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
