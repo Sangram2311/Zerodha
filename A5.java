293eba7e0c1225b7e4eb71f8dab5a29c68159b5a
Sangram Shinde <sangramshinde0105@gmail.com>
2022-05-25 18:48:32 +0530

    TC904 Added

---
+++ A5.java
@@ -0,0 +1,22 @@
+package string;
+
+public class A5 {
+	
+	public static void main(String[] args) {
+		
+		String str = "AutomationQuestion";
+		
+		for (int i=0; i<=3; i++) {
+			System.out.print(str.charAt(i));
+	//		System.out.println(str.length());
+		}
+		
+		System.out.println();
+		
+		for (int i=14; i<=17; i++) {
+			System.out.print(str.charAt(i));
+		}
+	
+	}
+
+}
